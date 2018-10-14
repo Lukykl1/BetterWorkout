95a0477ba28fe444750d6fa4b965094e1bf9ce4e
Lukykl1 <lukykl1@gmail.com>
2018-10-14 11:11:52 +0200

    Initial commit

---
+++ app/src/main/java/com/lukykl1/lukas/betterworkout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lukykl1.lukas.betterworkout
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
